5e7286c14eaf2f225b4d7bff09635fc059c8202a
Ahmed Reda <ahmedreda1009@gmail.com>
2024-05-06 00:26:43 +0300

    Modified: dashboard

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "tailwindcss", "eslint", "solargraph", "emmet_language_server" }
+local servers = { "html", "cssls", "tsserver", "tailwindcss", "eslint" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
