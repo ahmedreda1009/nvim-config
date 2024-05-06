01a11ccba4e8717504224ef8f77df35b706305a2
Ahmed Reda <ahmedreda1009@gmail.com>
2024-05-06 18:51:46 +0300

    Bug: attempt to make volar work but failed

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,7 +46,15 @@ return {
     },
     config = function()
       require("mason-lspconfig").setup {
-        ensure_installed = { "lua_ls" },
+        ensure_installed = {
+          "lua_ls",
+          "html",
+          "cssls",
+          "tsserver",
+          "tailwindcss",
+          "eslint",
+          "volar",
+        },
       }
     end,
   },
